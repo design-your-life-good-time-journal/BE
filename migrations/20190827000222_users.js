cdb15b245c45b4cb4e7c57c9ff0a7ce422dc1c2d
Taylor <wwwaaavvveeerrr@gmail.com>
2019-08-27 00:04:27 -0400

    created users table

---
+++ migrations/20190827000222_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', users =>{
+        users.increments();
+ 
+        users.string('firstname', 128);
+
+        users.string('lastname', 128);
+ 
+        users.datetime('date')
+ 
+    })
+ };
+ 
+ exports.down = function(knex, Promise) {
+     return knex.schema.dropTableIfExists('users');
+ };
